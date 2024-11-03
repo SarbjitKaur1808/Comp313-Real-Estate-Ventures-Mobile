1f4f176b6cdc7904662b3d22d0e6310fb8e2736a
sarbjit.kaur <sarbjit.kaur@pieinfosystems.com>
2024-11-02 20:22:49 -0400

    adding tabs

---
+++ RealEstateVentures/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+// src/pages/Home.tsx
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
---
+++ RealEstateVentures/src/pages/SignIn.tsx
@@ -0,0 +1,24 @@
+// src/pages/Settings.tsx
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function SignIn() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
